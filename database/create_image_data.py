994ac8266e1f99d2b83e4966d7dfdea729bdb524
JacobMJones <mrjacobtaiwan@gmail.com>
2023-11-23 08:19:31 -0500

    added subfolder support

--- database/create_image_data.py
+++ database/create_image_data.py
@@ -3,19 +3,17 @@ import sqlite3
 import uuid
 from PIL import Image, UnidentifiedImageError
 
-
-
-######### SCANS IMAGES folder and creates table
-
-
 # Path to your images folder
 images_folder = 'images'
+
+# Normalize the images_folder path
+images_folder = os.path.normpath(images_folder)
 
 # Connect to SQLite database
 conn = sqlite3.connect('database.db')
 cursor = conn.cursor()
 
-# Create the new table
+# Update the table creation statement to include 'path'
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS image_data (
         id TEXT PRIMARY KEY,
@@ -27,42 +25,48 @@ cursor.execute('''
         type TEXT,
         subtype TEXT,
         tags TEXT,
+        path TEXT,
         active INTEGER
     )
 ''')
 
-# List all files and directories in the images folder
-all_items = os.listdir(images_folder)
+# Traverse the directory, and list directories as dirs and files as files
+for dirpath, dirnames, files in os.walk(images_folder):
+    for file in files:
+        file_path = os.path.join(dirpath, file)
 
-# Process each item in the folder
-for item in all_items:
-    item_path = os.path.join(images_folder, item)
-
-    # Check if the item is a file
-    if os.path.isfile(item_path):
         try:
             # Extracting image details
-            with Image.open(item_path) as img:
+            with Image.open(file_path) as img:
                 width, height = img.size
                 format = img.format
 
-            size = os.path.getsize(item_path)
+            size = os.path.getsize(file_path)
 
             # Generating a UUID for each image
             unique_id = str(uuid.uuid4())
 
-            # Inserting the record into the new table
+            # Extracting folder name from dirpath
+            folder_name = os.path.basename(dirpath)
+
+            # Preparing tags, include folder name if it's not the root images_folder
+            tags = 'painting, surreal, Piet Mondrian'
+            if dirpath != images_folder:
+                tags += f', {folder_name}'
+
+            # Calculating the relative path
+            relative_path = os.path.relpath(file_path)
+
+            # Inserting the record into the new table, including the relative path
             cursor.execute(
-                "INSERT INTO image_data (id, file, width, height, size, format, type, subtype, tags, active) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-                (unique_id, item, width, height, size, format, 'image', 'art', 'painting, surreal, Piet Mondrian', 1)
+                "INSERT INTO image_data (id, file, width, height, size, format, type, subtype, tags, path, active) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                (unique_id, file, width, height, size, format, 'image', 'art', tags, relative_path, 1)
             )
 
-            print(f"Added {item} to image_data with ID {unique_id}.")
+            print(f"Added {file} to image_data with ID {unique_id}, path: {relative_path}.")
 
         except UnidentifiedImageError as e:
-            print(f"Error processing file {item}: {e}")
-    else:
-        print(f"Skipped directory {item}")
+            print(f"Error processing file {file}: {e}")
 
 # Commit the changes and close the connection
 conn.commit()
